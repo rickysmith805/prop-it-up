23697f28d3f5e92f7d44cd16ca97b0f7419d98c8
Ricky Smith <rickysmith@Rickys-MacBook-Pro.local>
2020-10-31 00:18:46 -0400

    birthday button

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 class PersonCard extends Component{
     constructor(props){
         super(props);
+        this.state= {
+            
+            age : this.props.person.age
+        }
 
 
     }
@@ -10,9 +14,14 @@ class PersonCard extends Component{
         return(
             <div className="card">
                 <div className="card-body">
-                    <h4 className="card-title">{this.props.person.lastName}, {this.props.person.firstName}</h4>
-                    <p className="card-text">Age: {this.props.person.age}</p>
+                    <h4 className="card-title text-center">{this.props.person.lastName}, {this.props.person.firstName}</h4>
+                    <p className="card-text">Age: {this.state.age}</p>
                     <p className="card-text">Hair Color: {this.props.person.hairColor}</p>
+                    <button 
+                    className="btn btn-outline-dark"
+                    onClick={() => this.setState({age:this.state.age +1})}
+                    >Birthday Button for {this.props.person.firstName}
+                    </button>
                 </div>
             </div>
         )
